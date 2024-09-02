d93199bd4403105e5699f0d7f52a53b6312df108
Cy5k0 <francisco.colomer@gmail.com>
2024-09-02 15:21:59 -0400

    update de los nombres de variables de alternativas.py

--- alternativa.py
+++ alternativa.py
@@ -4,19 +4,19 @@ class Alternativa:
         self._ayuda = ayuda
 
     # consulta el contenido de la alternativa
-    def get_contenido(self):
+    def getContenido(self):
         return self._contenido
 
     # modifica el contenido de la alternativa   
-    def set_contenido(self, nuevo_contenido):
+    def modificarContenido(self, nuevo_contenido):
         self._contenido = nuevo_contenido
 
     # consulta la ayuda de la alternativa
-    def get_ayuda(self):
+    def getAyuda(self):
         return self._ayuda
     
     # modifica la ayuda de la alterniatica
-    def set_ayuda(self, nueva_ayuda):
+    def modificarAyuda(self, nueva_ayuda):
         self._ayuda = nueva_ayuda
 
     
