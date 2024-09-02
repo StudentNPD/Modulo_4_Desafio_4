9f6dc08e86791f5635f1162c5cf92144aa78b3e8
Cy5k0 <francisco.colomer@gmail.com>
2024-09-02 19:30:10 -0400

    update pregunta.py agregar_alternativa.py

--- alternativa.py
+++ alternativa.py
@@ -7,22 +7,23 @@ class Alternativa:
     def getContenido(self):
         return self._contenido
 
-    # modifica el contenido de la alternativa   
+    # modifica el contenido de la alternativa
     def modificarContenido(self, nuevo_contenido):
         self._contenido = nuevo_contenido
 
     # consulta la ayuda de la alternativa
     def getAyuda(self):
         return self._ayuda
-    
+
     # modifica la ayuda de la alterniatica
     def modificarAyuda(self, nueva_ayuda):
         self._ayuda = nueva_ayuda
 
     # mostrar contenido y ayuda de la alternativa
     def mostrar(self):
-        muestra = (f"Contenido: {self._contenido}")
+        muestra = f"Contenido: {self._contenido}"
         if self._ayuda:
-            muestra += f"\nAyuda: {self._ayuda}" #coincatena la ayuda al contenido en una nueva linea
+            muestra += f"\nAyuda: {self._ayuda}"  # coincatena la ayuda al contenido en una nueva linea
         return muestra
-    #print(mostrar)
+
+    # print(mostrar)
--- pregunta.py
+++ pregunta.py
@@ -1,3 +1,6 @@
+from alternativa import Alternativa
+
+
 class Pregunta:
     def __init__(self, enunciado, esRequerida=False, ayuda=None):
         self._enunciado = enunciado
@@ -5,38 +8,38 @@ class Pregunta:
         self._ayuda = ayuda
         self._alternativas = []
 
-
-    #consulta el enunciado de la pregunta
+    # consulta el enunciado de la pregunta
     def getEnunciado(self):
         return self._enunciado
 
-    #modifica el enunciado de la pregunta
+    # modifica el enunciado de la pregunta
     def modificarEnunciado(self, nuevo_enunciado):
         self._enunciado = nuevo_enunciado
 
-    #consulta la ayuda de la pregunta
+    # consulta la ayuda de la pregunta
     def getAyuda(self):
         return self._ayuda
 
-    #modifica la ayuda de la pregunta
+    # modifica la ayuda de la pregunta
     def modificarAyuda(self, nuevo_ayuda):
         self._ayuda = nuevo_ayuda
 
-    #consulta si es requerida la pregunta (ojo es boleano)
+    # consulta si es requerida la pregunta (ojo es boleano)
     def getEsRequerida(self):
         return self._esRequerida
 
-    #modifica si es requerida la pregunta (ojo es boleano)
+    # modifica si es requerida la pregunta (ojo es boleano)
     def setEsRequerida(self, esRequerida):
         self._es_requerida = esRequerida
 
+    # consulta las alternativas de la pregunta (tipo lista)
+    def agregar_alternativa(self, alternativa_dict):
+        alternativa = Alternativa(
+            alternativa_dict["contenido"], alternativa_dict.get("ayuda")
+        )
+        self._alternativas.append(alternativa)
 
-    #consulta las alternativas de la pregunta (tipo lista)
-    def getAlternativas(self):
-        return self._alternativas
+    # def agregar_alternativa ???
 
-    #def agregar_alternativa ???
-
-
-    #Muestra la pregunta, su enunciado, ayuda y sus alternativas
-    #def mostrar
+    # Muestra la pregunta, su enunciado, ayuda y sus alternativas
+    # def mostrar(self)
