d934c292eea8b73faf6f07ccb5fcae706b945b6d
fmonroy75 <83427525+fmonroy75@users.noreply.github.com>
2024-09-02 20:55:19 -0400

    Update usuario.py

--- usuario.py
+++ usuario.py
@@ -3,3 +3,16 @@ class Usuario:
         self.__correo = correo
         self.__edad = edad
         self.__region = region
+
+    @property
+    def correo(self) -> str:
+        return self.__correo
+
+    @property
+    def edad(self) -> int:
+        return self.__edad
+
+    @property
+    def region(self) -> int:
+        return self.__region
+
