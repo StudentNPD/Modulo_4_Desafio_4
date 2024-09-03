d70ef5083d061b5be34dce15d8a77d492b7cccb1
fmonroy75 <83427525+fmonroy75@users.noreply.github.com>
2024-09-02 20:57:09 -0400

    Update usuario.py

--- usuario.py
+++ usuario.py
@@ -16,5 +16,16 @@ class Usuario:
     def getRegion(self) -> int:
         return self.__region
 
+    @correo.setter
+    def modificarCorreo(self, nuevo_correo: str) -> None:
+        self.__correo = nuevo_correo
+
+    @edad.setter
+    def modificarEdad(self, nueva_edad: int) -> None:
+        self.__edad = nueva_edad
+
+    @region.setter
+    def modificarRegion(self, nueva_region: int) -> None:
+        self.__region = nueva_region
 
 
